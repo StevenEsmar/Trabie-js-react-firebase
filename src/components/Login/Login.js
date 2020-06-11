27cdc42d7f1cf155f6efe869a9e3d378851dc4e2
StevenEsmar <stespinosama@unal.edu.co>
2020-06-11 02:08:34 -0500

    Start project

---
+++ src/components/Login/Login.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Login.css';
+
+class login extends React.Component {
+    render(){
+        return(
+            
+            <div className='container'>
+                
+             
+                Traspaso de bienes educativos en la Universidad nacional
+               
+            </div>
+        )
+
+    }
+}
+
+export default login;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {BrowserRouter,Route,Switch,Redirect} from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Login from './components/Login/Login'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const root = (
+
+    <BrowserRouter>
+      <Switch>
+        <Route path="/Login" component={Login}/>
+        <Redirect from="/" to  ="/Login"/>
+      </Switch>
+    </BrowserRouter>
+
 );
+
+ReactDOM.render(root, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//serviceWorker.unregister();
